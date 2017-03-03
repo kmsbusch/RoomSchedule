a20c19f62ef4e7275382557449422e87f03c7aad
busmic1206 <mbusch@coloradotech.edu>
2017-03-02 22:03:21 -0700

    Data from form has escapes: '\\'414'\\' need to peel those off.

--- RoomSchedule.py
+++ RoomSchedule.py
@@ -1,5 +1,5 @@
 from tinydb import TinyDB, Query
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 #from flask.ext.pymongo import PyMongo
 
 # client = MongoClient('localhost:27017')
@@ -21,9 +21,40 @@ db = TinyDB('C:\\Users\\busmic1206\\RoomSchedule\\data\\room_data.json ')
 # def hello_world():
 #     return 'Hello World!'
 
+default_room_data = {
+    'number':'414',
+    'quarter':'1701',
+    'times':{
+        '9:00':['']*6,
+        '10:00':['']*6,
+        '11:00':['']*6,
+        '12:00':['']*6,
+        '1:00':['']*6,
+        '2:00':['']*6,
+        '2:30':['']*6,
+        '3:00':['']*6,
+        '4:00':['']*6,
+        '5:30':['']*6,
+        '6:00':['']*6,
+        '7:00':['']*6,
+        '7:30':['']*6,
+        '8:00':['']*6,
+        '9:00':['']*6,
+        '10:00':['']*6
+    }
+}
+
 @app.route('/')
 def index():
-    return render_template('room_table.html')
+    return render_template('choose_room.html')
+
+@app.route('/get_schedule', methods=['POST'])
+def get_schedule():
+    Room = Query()
+    room = request.form.get('rooms')
+    quarters = request.form.get('quarters')
+    data = db.search(Room.room == room and Room.quarter == quarters)
+    return render_template('room_table.html', data=data)
 
 @app.route('/rooms', methods=['GET'])
 def get_rooms():
